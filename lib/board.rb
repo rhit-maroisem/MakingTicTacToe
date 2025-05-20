cab84a76838e43cff226a6970b030c6d42f47ed7
Emile Marois <maroisem@rose-hulman.edu>
2025-05-20 11:36:46 -0400

    test_board_rep_init passes

--- lib/board.rb
+++ lib/board.rb
@@ -4,9 +4,15 @@ class Board
   def initialize(ui, board_size)
     @ui = ui
     @board_size = board_size
+    @board_rep = Array.new(board_size) {Array.new(board_size) {""}}
   end
 
   def get_size
     @board_size
   end
+
+  def get_rep
+    @board_rep
+  end
+
 end
--- tests/board_test.rb
+++ tests/board_test.rb
@@ -20,4 +20,15 @@ class BoardTest < Minitest::Test
     board2 = Board.new(ui, 4)
     assert_equal(4, board2.get_size)
   end
+
+  def test_board_rep_init
+    ui = Ui.new
+    board = Board.new(ui, 3)
+    assert_equal(3, board.get_size)
+    rep_array = board.get_rep
+    assert_equal(3, rep_array.length)
+    assert_equal(3, rep_array[0].length)
+    rep_array.each {|arr| arr.all? {|entry| assert_equal("",entry)} }
+
+  end
 end
